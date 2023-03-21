f88f4b310f35ef3241b12f16a96f063a0c8f4173
liliebingham <69400913+liliebingham@users.noreply.github.com>
2023-03-20 17:25:38 -0700

    hello circle

---
+++ hello-circle/hello-circle/src/main/java/com/company/hellocircle/controller/HelloCircleController.java
@@ -0,0 +1,18 @@
+package com.company.hellocircle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloCircleController {
+
+    @GetMapping(value = "/hello")
+    public String helloCircle(){
+        return "Hello, Circle!";
+    }
+
+    @GetMapping(value = "/answer")
+    public String getAnswer(){
+        return "The answer is 42!";
+    }
+}
